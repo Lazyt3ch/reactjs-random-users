836ce2fdc724ece083feba5a18be2c4f65f38bf3
Rashid <Lazyt3ch@gmail.com>
2020-11-30 15:56:27 +0500

    working on users grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React, {setState} from "react";
+import UsersGrid from "/Components/UsersGrid.js";
 import './App.css';
 
 function App() {
+  const [numResults, setNumResults] = setState(0);
+  const [properties, setProperties] = setState("");
   return (
-    <div>
-
-    </div>
+    <UsersGrid numResults={numResults} properties={properties} />
   );
 }
 
---
+++ src/Components/UsersGrid.js
@@ -0,0 +1,14 @@
+import React, {setState} from "react";
+
+function UsersGrid(props) {
+  const usersPerPageDefault = 20;
+  const [usersPerPage, setUsersPerPage] = setState(usersPerPageDefault);
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default UsersGrid;
