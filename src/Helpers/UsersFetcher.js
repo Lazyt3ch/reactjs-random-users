a0b9cb1f0683e0e6e424c111536d74bddc8539a8
Rashid <Lazyt3ch@gmail.com>
2020-11-30 14:23:50 +0500

    working on users fetcher

---
+++ src/Helpers/UsersFetcher.js
@@ -0,0 +1,39 @@
+const usersFetcher = (numResults, params=[]) => {
+  const validParamsAsOneString = `
+    gender
+    name
+    location
+    email
+    login
+    registered
+    dob
+    phone
+    cell
+    id
+    picture
+    nat
+  `;
+
+  const validParams = validParamsAsOneString.trim().split(/[\s+]/);
+
+  const numResultsLowerLimit = 5;
+  const numResultsUpperLimit = 1000;
+  const numResultsDefault = 100;
+  let numResultsToUse;
+
+  if (typeof numResults === "number") {
+    numResultsToUse = numResults;
+  } else if (typeof numResults === "string") {
+    numResultsToUse = parseInt(numResults);
+  } else {
+    numResultsToUse = numResultsDefault;
+  }
+
+  if (numResults < numResultsLowerLimit) {
+    numResults = numResultsLowerLimit;
+  }
+
+  const baseUrl = https://randomuser.me/api/;
+};
+
+export default usersFetcher;
