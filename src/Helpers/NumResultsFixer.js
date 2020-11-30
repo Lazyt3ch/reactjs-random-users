272e777574c48b852701af0433c14368a893d2a1
Rashid <Lazyt3ch@gmail.com>
2020-11-30 15:09:18 +0500

    made dummy change

--- src/Helpers/NumResultsFixer.js
+++ src/Helpers/NumResultsFixer.js
@@ -1,20 +1,18 @@
 const fixNumResults = (numResults) => {
   const numResultsLowerLimit = 5;
   const numResultsUpperLimit = 1000;
-  const numResultsDefault = 100;
+  const numResultsDefault = 50;
+
   let numResultsToUse = numResultsDefault;
   
-  if (typeof numResults === "number") {
+  if (Number.isInteger(numResults)) {
     numResultsToUse = numResults;
-  } else if (typeof numResults === "string") {
-    numResultsToUse = parseInt(numResults);
-  }
-  
-  if (numResultsToUse < numResultsLowerLimit) {
-    numResultsToUse = numResultsLowerLimit;
-  } else if (numResultsToUse > numResultsUpperLimit) {
-    numResultsToUse = numResultsUpperLimit;
-  }
+    if (numResultsToUse < numResultsLowerLimit) {
+      numResultsToUse = numResultsLowerLimit;
+    } else if (numResultsToUse > numResultsUpperLimit) {
+      numResultsToUse = numResultsUpperLimit;
+    }
+  }  
 
   return numResultsToUse;    
 }
