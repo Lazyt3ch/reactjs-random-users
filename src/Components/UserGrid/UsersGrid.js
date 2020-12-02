36b244e7c5888984ee9108e971b648e6f4bb4536
Rashid <Lazyt3ch@gmail.com>
2020-12-02 16:53:38 +0500

    working on user properties rebuilding for viewing

--- src/Components/UserGrid/UsersGrid.js
+++ src/Components/UserGrid/UsersGrid.js
@@ -7,11 +7,25 @@ function UsersGrid(props) {
 
   const usersPerPageDefault = 20;
   const [usersPerPage, setUsersPerPage] = useState(usersPerPageDefault);
-  const rebultResults = buildResults(results);
+  const rebuiltResults = buildResults(results);
   // console.log("rebultResults =", rebultResults);
 
   return (
     <div>
+
+      <div>
+        {rebuiltResults && rebuiltResults.length 
+          && Object.keys(rebuiltResults[0]).map( key =>
+            <span key={key}>{key}</span> )
+        }
+      </div>
+      
+      {rebuiltResults && rebuiltResults.length 
+        && rebuiltResults.map( userObj => 
+          Object.values(userObj).map( value => 
+            <span key={value}>{value}</span> )
+        )                
+      }
 
     </div>
   );
