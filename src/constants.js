cdbcf1fbd25188bcd13b2a41faa4a002e9dcd0e5
Rashid <Lazyt3ch@gmail.com>
2020-12-02 14:10:38 +0500

    working on user properties rebuilding for viewing

--- src/constants.js
+++ src/constants.js
@@ -16,13 +16,59 @@ const constants = {
     picture
     nat
   `,
-  propertyPieces = {
-    name: ["title", "first", "last"],
-    gender: [],
-    coordinates: ["latitude", "longitude"],
-  
+  propertyPieces: {
+    gender: null,
+    name: {
+      title: null, 
+      first: null, 
+      last: null,
+    },
+    location: {
+      street: {
+        number: null,
+        name: null,
+      },
+      city: null,
+      state: null,
+      country: null,
+      postcode: null,
+      coordinates: {
+        latitude: null, 
+        longitude: null,
+      },
+    },
+    email: null,
+    login: {
+      uuid: null,
+      username: null,
+      password: null,
+      salt: null,
+      md5: null,
+      sha1: null,
+      sha256: null,      
+    },
+    dob: {
+      date: null,
+      age: null,
+    },
+    registered: {
+      date: null,
+      age: null,
+    },
+    phone: null,
+    cell: null,
+    id: {
+      name: null,
+      value: null,
+    },
+    picture: {
+      large: null,
+      medium: null,
+      thumbnail: null,
+    },
+    nat: null,
   },
-  
-}
+ 
+};
 
 export default constants;
