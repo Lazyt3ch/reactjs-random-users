587f08c01e31d7c4a90918e399a0143df30e4d21
Rashid <Lazyt3ch@gmail.com>
2020-12-02 12:30:39 +0500

    added navlink styles

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,22 +1,45 @@
 import React from "react";
 import {
-  Link
+  NavLink
 } from "react-router-dom";
 
 function Nav(props) {
+  const navLinkStyle = {
+    textDecoration: "none",
+  };
+
   return (
     <nav>
-      <ul className="nav">
+      <ul className="nav-ul">
         <li>
-          <Link to="/get">Data getter</Link>
+          <NavLink 
+            to="/get" 
+            style={navLinkStyle}
+            activeClassName="nav-link-active"
+          >
+            Data getter
+          </NavLink>
         </li>
 
         <li>
-          <Link to="/view">Data viewer</Link>
+          <NavLink 
+            to="/view" 
+            style={navLinkStyle}
+            activeClassName="nav-link-active"
+          >
+            Data viewer
+          </NavLink>
         </li>
 
         <li>
-          <Link to="/">Home page</Link>        </li>
+          <NavLink 
+            exact to="/" 
+            style={navLinkStyle}
+            activeClassName="nav-link-active"
+          >
+            Home page
+          </NavLink>        
+        </li>
 
       </ul>
     </nav>
