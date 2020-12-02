4ea7b7b5bfc67babc7f13448f92eebf6164ce1a0
Rashid <Lazyt3ch@gmail.com>
2020-12-02 16:10:18 +0500

    working on user properties rebuilding for viewing

--- src/Helpers/DataRebuilder.js
+++ src/Helpers/DataRebuilder.js
@@ -4,33 +4,39 @@ const getRebuiltData = userObj => {
   console.log("userObj =", userObj);
   // let builtArr = [];
   const builtObj = {};
+  const builtStr = "";
 
   if (typeof userObj !== 'object') {
     return builtObj;
   }
 
-  const extractData = (currentObj, level=1, subArr=[]) => {
-    let hitBottom = false;
+  // const extractData = (currentObj, level=1, subArr=[]) => {
+  const extractData = (currentObj, level=1, builtStr="") => {
+      let hitBottom = false;
 
     Object.entries(currentObj).forEach( ([key, value]) => {
-      console.log("key, value =", key, value)
+      // console.log("key, value =", key, value)
 
       if (typeof value === 'object') {
-        subArr.push( `${key}: (` );
-        extractData(value, level + 1, subArr);
+        // subArr.push( `${key}: (` );
+        // extractData(value, level + 1, subArr);
+        extractData(value, level + 1, `${builtStr.length ? builtStr + ", " : ""}${key}: (` );
       } else {
-        subArr.push( `${key}: ${value}` );
+        // subArr.push( `${key}: ${value}` );
+        builtStr += `${key}: ${value}, `;
         hitBottom = true;
       }
     });
 
     if (hitBottom) {
       if (level > 1) {
-        subArr.push( ")".repeat(level - 1) );
+        // subArr.push( ")".repeat(level - 1) );
+        builtStr = `${builtStr.endsWith(", ") ? builtStr.slice(-2) : builtStr}${")".repeat(level - 1)}`;
       }
     }
     
-    return subArr;
+    // return subArr;
+    return builtStr;
   }
 
   Object.entries(userObj).forEach( ([key, value]) => {
@@ -39,6 +45,10 @@ const getRebuiltData = userObj => {
   
   // extractData(userObj);
   console.log("builtObj =", builtObj);
+
+  // for (const part of builtObj)
+
+
   return builtObj;
 }
 
