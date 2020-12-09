33fe461fbe42dd1a7fdfb6710289c0cb45be55a8
Rashid <Lazyt3ch@gmail.com>
2020-12-09 10:52:08 +0500

    slightly changed text in home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,16 +4,16 @@ function Home() {
   return (
     <div className="home-page">
       <h1>Hello!</h1>
-      <p>You can use this simple web app to retrieve specific users data from RandomUser.me</p>
+      <p>You can use this simple web app to retrieve and view users data from RandomUser.me</p>
 
       <p><strong>Technical notes</strong></p>
       <ul>
         <li>
-          <p>For some reason, when <strong>user id</strong> is requested, RandomUser.me may return 
+          <p>For some reason, when a <strong>user id</strong> is requested, RandomUser.me may return 
           an invalid name&mdash;value pair like this:</p>
           <p><strong>id: &#x7B;name: "", value: null&#x7D;</strong></p>
-          <p>In such cases, when displayed in the Data Viewer, the empty name and null value are replaced 
-          with a &lt;null&gt;&mdash;&lt;/null&gt; pair.</p>
+          <p>In such cases, when data is displayed in Data Viewer, each empty name and null value 
+            will be replaced with a &lt;null&gt;&mdash;&lt;/null&gt; pair.</p>
         </li>
 
         <li>
