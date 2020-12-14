09473d6dd4da71a3bbb0ff1e38be9aba939596d2
Rashid <Lazyt3ch@gmail.com>
2020-12-14 11:18:05 +0500

    added an extra error check to data retriever

--- src/Helpers/UsersFetcher.js
+++ src/Helpers/UsersFetcher.js
@@ -7,14 +7,21 @@ async function fetchUsers(numResults, properties=[], isToInclude=true) {
 
   try {
     const response = await fetch(completeUrl);
-    const data = await response.json();
-    // console.log("data =", data);
 
-    if (data.error || !data.results || !data.results.length) {
-      errorMessage = "The server returned an unspecified error.";
-      console.log(errorMessage);      
+    // https://www.tjvantoll.com/2015/09/13/fetch-and-errors/
+    if (!response.ok) {
+      errorMessage = `An error occurred: ${response.statusText}`;
+      console.log(errorMessage);
     } else {
-      resultsArr = data.results;
+      const data = await response.json();
+      // console.log("data =", data);
+  
+      if (data.error || !data.results || !data.results.length) {
+        errorMessage = "The server returned an unspecified error.";
+        console.log(errorMessage);      
+      } else {
+        resultsArr = data.results;
+      }  
     }
   } catch(err) {
     errorMessage = `An error occurred: ${err}`;
