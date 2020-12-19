e11a10b725cdce000dd4601adf679e953afbdda6
Rashid <Lazyt3ch@gmail.com>
2020-12-19 14:43:02 +0500

    working on nav

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,33 +1,65 @@
 import {React} from "react";
 import {NavLink} from "react-router-dom";
-import {ListItem} from '@material-ui/core';
+import {AppBar} from '@material-ui/core';
 
 function Nav(props) {
   const {activePageNumber} = props;
 
   return (
-    <nav className="nav-container">
-      <ListItem button component={NavLink} 
-        to="/get" exact
-        activeClassName="Mui-selected" 
-      >
-        Data Retriever
-      </ListItem>
+    <>
+      <AppBar position="static">
+        <div className="nav-container">
+          <NavLink 
+            to="/get" exact
+            className="nav-link"
+            activeClassName="nav-link-active" 
+          >
+            Data Retriever
+            </NavLink>
 
-      <ListItem button component={NavLink} 
-        to={`/view/${activePageNumber + 1}`} 
-        activeClassName="Mui-selected" 
-      >
-        Data Viewer
-      </ListItem>
+          <NavLink
+            to={`/view/${activePageNumber + 1}`} 
+            className="nav-link"
+            activeClassName="nav-link-active" 
+          >
+            Data Viewer
+          </NavLink>
 
-      <ListItem button component={NavLink} 
-        to="/" exact
-        activeClassName="Mui-selected" 
-      >
-        Home Page
-      </ListItem>        
-    </nav>    
+          <NavLink 
+            to="/" exact
+            className="nav-link"
+            activeClassName="nav-link-active" 
+          >
+            Home Page
+          </NavLink>  
+        </div>
+      </AppBar>
+
+
+
+      {/* <nav className="nav-container">
+        <ListItem button component={NavLink} 
+          to="/get" exact
+          activeClassName="Mui-selected" 
+        >
+          Data Retriever
+        </ListItem>
+
+        <ListItem button component={NavLink} 
+          to={`/view/${activePageNumber + 1}`} 
+          activeClassName="Mui-selected" 
+        >
+          Data Viewer
+        </ListItem>
+
+        <ListItem button component={NavLink} 
+          to="/" exact
+          activeClassName="Mui-selected" 
+        >
+          Home Page
+        </ListItem>        
+      </nav>     */}
+    </>
   );
 }
 
