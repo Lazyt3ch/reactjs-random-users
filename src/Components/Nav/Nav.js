6589367e570091621645ee4a31d39a913cf60187
Rashid <Lazyt3ch@gmail.com>
2020-12-19 14:09:39 +0500

    cleaned up

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -28,40 +28,6 @@ function Nav(props) {
         Home Page
       </ListItem>        
     </nav>    
-
-      // <ul className="nav-ul">
-      //   <li>
-      //     <NavLink 
-      //       to="/get" 
-      //       className="nav-link unselectable"
-      //       activeClassName="nav-link-active"
-      //     >
-      //       Data Retriever
-      //     </NavLink>
-      //   </li>
-
-      //   <li>
-      //     <NavLink 
-      //       to={`/view/${activePageNumber + 1}`} 
-      //       className="nav-link unselectable"
-      //       activeClassName="nav-link-active"
-      //     >
-      //       Data Viewer
-      //     </NavLink>
-      //   </li>
-
-      //   <li>
-      //     <NavLink 
-      //       exact to="/" 
-      //       className="nav-link unselectable"
-      //       activeClassName="nav-link-active"
-      //     >
-      //       Home Page
-      //     </NavLink>        
-      //   </li>
-
-      // </ul>
-    // </nav>
   );
 }
 
