035754085c3709cb5642ec1ef5aee1a0c1d588a1
Rashid <Lazyt3ch@gmail.com>
2020-12-19 14:08:51 +0500

    working on nav

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,44 +1,67 @@
 import {React} from "react";
 import {NavLink} from "react-router-dom";
+import {ListItem} from '@material-ui/core';
 
 function Nav(props) {
   const {activePageNumber} = props;
 
   return (
     <nav className="nav-container">
-      <ul className="nav-ul">
-        <li>
-          <NavLink 
-            to="/get" 
-            className="nav-link unselectable"
-            activeClassName="nav-link-active"
-          >
-            Data Retriever
-          </NavLink>
-        </li>
+      <ListItem button component={NavLink} 
+        to="/get" exact
+        activeClassName="Mui-selected" 
+      >
+        Data Retriever
+      </ListItem>
 
-        <li>
-          <NavLink 
-            to={`/view/${activePageNumber + 1}`} 
-            className="nav-link unselectable"
-            activeClassName="nav-link-active"
-          >
-            Data Viewer
-          </NavLink>
-        </li>
+      <ListItem button component={NavLink} 
+        to={`/view/${activePageNumber + 1}`} 
+        activeClassName="Mui-selected" 
+      >
+        Data Viewer
+      </ListItem>
 
-        <li>
-          <NavLink 
-            exact to="/" 
-            className="nav-link unselectable"
-            activeClassName="nav-link-active"
-          >
-            Home Page
-          </NavLink>        
-        </li>
+      <ListItem button component={NavLink} 
+        to="/" exact
+        activeClassName="Mui-selected" 
+      >
+        Home Page
+      </ListItem>        
+    </nav>    
 
-      </ul>
-    </nav>
+      // <ul className="nav-ul">
+      //   <li>
+      //     <NavLink 
+      //       to="/get" 
+      //       className="nav-link unselectable"
+      //       activeClassName="nav-link-active"
+      //     >
+      //       Data Retriever
+      //     </NavLink>
+      //   </li>
+
+      //   <li>
+      //     <NavLink 
+      //       to={`/view/${activePageNumber + 1}`} 
+      //       className="nav-link unselectable"
+      //       activeClassName="nav-link-active"
+      //     >
+      //       Data Viewer
+      //     </NavLink>
+      //   </li>
+
+      //   <li>
+      //     <NavLink 
+      //       exact to="/" 
+      //       className="nav-link unselectable"
+      //       activeClassName="nav-link-active"
+      //     >
+      //       Home Page
+      //     </NavLink>        
+      //   </li>
+
+      // </ul>
+    // </nav>
   );
 }
 
