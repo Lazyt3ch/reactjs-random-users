a0626d85e44f69c08264104c0fd5b2633eb2c874
Rashid <Lazyt3ch@gmail.com>
2020-12-20 16:44:55 +0500

    cleaned up bad data checker

--- src/Helpers/BadDataChecker.js
+++ src/Helpers/BadDataChecker.js
@@ -1,10 +1,8 @@
 function isBadData() {
   // Makes sure that data is non-empty arrays
   const args = Array.prototype.slice.call(arguments);
-  // console.log("args =", args);
   for (const arg of args) {
     if (!arg || !Array.isArray(arg) || !arg.length) {
-      // console.log("BAD DATA: arg =", arg);
       return true;
     }
   }
