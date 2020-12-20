320b91918bb24d4708afac833b0629beec247152
Rashid <Lazyt3ch@gmail.com>
2020-12-20 16:40:53 +0500

    simplified grid columns formula

--- src/Helpers/GridCalculator.js
+++ src/Helpers/GridCalculator.js
@@ -1,59 +1,69 @@
 import isBadData from "./BadDataChecker.js";
 
-// Reserve some horizontal space to prevent overflow
-const maxTotalWidth = 90; 
+// // Reserve some horizontal space to prevent overflow
+// const maxTotalWidth = 90; 
 
-const getColumnWidths = results => {
-  if (isBadData(results)) {
-    return [];
-  }
+// const getColumnWidths = results => {
+//   if (isBadData(results)) {
+//     return [];
+//   }
 
-  // Let 1% be the minimum allowed column width
-  let absWidths = new Array(results[0].length).fill(1); 
+//   // Let 1% be the minimum allowed column width
+//   let absWidths = new Array(results[0].length).fill(1); 
 
-  results.forEach( rowArr => {
-    rowArr.forEach( (cellStr, cellIdx) => {
-      let width = absWidths[cellIdx] || 1;
-      let cellStrLen = typeof cellStr === "string" ? cellStr.length : 1;
-      absWidths[cellIdx] = Math.max(width, cellStrLen);
-    });
-  });
+//   results.forEach( rowArr => {
+//     rowArr.forEach( (cellStr, cellIdx) => {
+//       let width = absWidths[cellIdx] || 1;
+//       let cellStrLen = typeof cellStr === "string" ? cellStr.length : 1;
+//       absWidths[cellIdx] = Math.max(width, cellStrLen);
+//     });
+//   });
 
-  const relWidths = absWidths.map( (w, idx) => Math.max(1, Math.sqrt(w)) );
+//   const relWidths = absWidths.map( (w, idx) => Math.max(1, Math.sqrt(w)) );
   
-  const sumOfWidths = relWidths.reduce( (acc, w) => acc + w, 0 );
+//   const sumOfWidths = relWidths.reduce( (acc, w) => acc + w, 0 );
 
-  relWidths.forEach( (w, idx, arr) => 
-    arr[idx] = Math.max(1, Math.floor(maxTotalWidth * w / sumOfWidths)) );
+//   relWidths.forEach( (w, idx, arr) => 
+//     arr[idx] = Math.max(1, Math.floor(maxTotalWidth * w / sumOfWidths)) );
 
-  const columnWidths = absWidths.map( (absWidth, idx) => 
-    ({abs: absWidth, rel: relWidths[idx]}) );
+//   const columnWidths = absWidths.map( (absWidth, idx) => 
+//     ({abs: absWidth, rel: relWidths[idx]}) );
 
-  return columnWidths;
-};
+//   return columnWidths;
+// };
+
+// const getGridColumnsFormula = (results2D) => {
+//   if (isBadData(results2D)) {
+//       return "";
+//   }
+
+//   let columnWidths;
+
+//   try {
+//     columnWidths = getColumnWidths(results2D);
+//   } catch (err) {
+//     return "";
+//   }
+
+//   const gridColumnsFormulaArr = columnWidths.map( (width, idx) => {
+//     // console.log("columnWidths =", columnWidths);
+//     // const absWidth = width["abs"];
+//     const relWidth = width["rel"];
+//     return `minmax(${relWidth}%, max-content)`;  
+//   });
+
+//   const gridColumnsFormula = gridColumnsFormulaArr.join(" ");  
+//   return gridColumnsFormula;
+// };
 
 const getGridColumnsFormula = (results2D) => {
-  if (isBadData(results2D)) {
+  if (isBadData(results2D) || isBadData(results2D[0])) {
       return "";
   }
 
-  let columnWidths;
-
-  try {
-    columnWidths = getColumnWidths(results2D);
-  } catch (err) {
-    return "";
-  }
-
-  const gridColumnsFormulaArr = columnWidths.map( (width, idx) => {
-    // console.log("columnWidths =", columnWidths);
-    // const absWidth = width["abs"];
-    const relWidth = width["rel"];
-    return `minmax(${relWidth}%, max-content)`;  
-  });
-
-  const gridColumnsFormula = gridColumnsFormulaArr.join(" ");  
-  return gridColumnsFormula;
+  return results2D[0]
+    .map( _ => "minmax(min-content, max-content)" )
+    .join(" ");
 };
 
 export default getGridColumnsFormula;
