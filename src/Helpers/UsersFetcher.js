008e8c393ec338fcf455e0bbbbf8d0f5e4debd59
Rashid <Lazyt3ch@gmail.com>
2020-12-20 16:46:43 +0500

    cleaned up users fetcher

--- src/Helpers/UsersFetcher.js
+++ src/Helpers/UsersFetcher.js
@@ -2,7 +2,6 @@ import buildUrl from "./UrlBuilder.js";
 
 async function fetchUsers(numResults, properties=[], isToInclude=true) {
   const completeUrl = buildUrl(numResults, properties, isToInclude);
-  // console.log("completeUrl =", completeUrl);
   let resultsArr = null;
   let errorMessage = "";
 
@@ -12,14 +11,12 @@ async function fetchUsers(numResults, properties=[], isToInclude=true) {
     // https://www.tjvantoll.com/2015/09/13/fetch-and-errors/
     if (response.ok) {
       const data = await response.json();
-      // console.log("data =", data);
   
       if (data.error || !data.results || !data.results.length) {
         errorMessage = "The server returned an unspecified error.";
         console.log(errorMessage);      
       } else {
         resultsArr = data.results;
-        // console.log("resultsArr =", resultsArr);
       }  
     } else {
       errorMessage = `An error occurred: ${response.statusText}`;
