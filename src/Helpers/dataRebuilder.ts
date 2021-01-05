64421602e19971cceec9e804867c5ee8bb810135
Rashid <Lazyt3ch@gmail.com>
2021-01-05 15:39:54 +0500

    working on dataRebuilder

--- src/Helpers/dataRebuilder.ts
+++ src/Helpers/dataRebuilder.ts
@@ -6,45 +6,56 @@ const addTrailingCommaSpace = (str: string): string => {
   return str.endsWith(", ") ? str : `${str}, `;
 };
 
-const getRebuiltData = (userObj: object | null | undefined): object => {
+
+const extractData = (currentObj: object, level=1): string => {
+  let hitBottom = false;
+
+  Object.entries(currentObj).forEach( ([key, value]) => {
+    if (value !== null && typeof value === 'object') {
+      builtStr = `${builtStr.length ? addTrailingCommaSpace(builtStr) : ""}${key}: [`;
+      extractData(value, level + 1);
+    } else {
+      // <undefined> will replace an empry string, and <null> will replace a null value
+      builtStr = `${builtStr}${key.length ? key : "<undefined>"}: ${value ? value: "<null>"}, `;
+      hitBottom = true;
+    }
+  });
+
+  if (hitBottom) {
+    if (level > 1) {
+      builtStr = `${removeTrailingCommaSpace(builtStr)}${"]".repeat(level - 1)}, `;
+    }
+  }
+  
+  return builtStr;
+}
+
+const getRebuiltData = (userObj: object | null | undefined) => {
   // Now using brackets instead of parentheses as subproperty grouping characters,
   // because parentheses sometimes occur in retrieved users data
-  const builtObj = {};
   let builtStr: string;
 
-  if (typeof userObj !== 'object') {
+  if (userObj === null || userObj === undefined) {
     return {};
   }
 
-  const extractData = (currentObj: object, level=1): string => {
-    let hitBottom = false;
 
-    Object.entries(currentObj).forEach( ([key, value]) => {
-      if (value !== null && typeof value === 'object') {
-        builtStr = `${builtStr.length ? addTrailingCommaSpace(builtStr) : ""}${key}: [`;
-        extractData(value, level + 1);
-      } else {
-        // <undefined> will replace an empry string, and <null> will replace a null value
-        builtStr = `${builtStr}${key.length ? key : "<undefined>"}: ${value ? value: "<null>"}, `;
-        hitBottom = true;
-      }
-    });
 
-    if (hitBottom) {
-      if (level > 1) {
-        builtStr = `${removeTrailingCommaSpace(builtStr)}${"]".repeat(level - 1)}, `;
-      }
-    }
-    
-    return builtStr;
-  }
+  const builtObj = {};
 
   if (typeof userObj === 'object') {
     Object.entries(userObj).forEach( ([key, value]) => {
       builtStr = "";
-      builtObj[key] = typeof value === 'object' 
-        ? removeTrailingCommaSpace(extractData(value))
-        : value;
+
+      if (value !== null && typeof value === 'object') {
+        builtObj[key] = removeTrailingCommaSpace(extractData(value))
+      } else {
+        builtObj[key] = value;
+      }
+
+      // builtObj[key] = (value !== null && typeof value === 'object')
+      //   ? removeTrailingCommaSpace(extractData(value))
+      //   : value;
     });
     
     return builtObj;
