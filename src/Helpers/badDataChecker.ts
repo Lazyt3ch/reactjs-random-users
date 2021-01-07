a1ec3a6f36edcde54d0f65280f020d136f4d888d
Rashid <Lazyt3ch@gmail.com>
2021-01-07 12:19:35 +0500

    cleaned up badDataChecker

--- src/Helpers/badDataChecker.ts
+++ src/Helpers/badDataChecker.ts
@@ -1,17 +1,5 @@
-// function isBadData(...args: any[]) {
-//     // Makes sure that data is non-empty arrays
-//   for (const arg of args) {
-//     if (!arg || !Array.isArray(arg) || !arg.length) {
-//       return true;
-//     }
-//   }
-  
-//   return false;
-// }; 
-
 function isNonEmptyArray(item: unknown): boolean {
   return (Array.isArray(item) && item.length > 0);
 }
 
-// export default isBadData;
 export default isNonEmptyArray;
