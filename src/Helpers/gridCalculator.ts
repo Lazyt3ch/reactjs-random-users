71c2f9eadbc4326f3b196ad639a11a03d1344e43
Rashid <Lazyt3ch@gmail.com>
2021-01-07 12:13:44 +0500

    working on gridCalculator

--- src/Helpers/gridCalculator.ts
+++ src/Helpers/gridCalculator.ts
@@ -1,6 +1,6 @@
 import isBadData from "./badDataChecker";
 
-const getGridColumnsFormula = (results2D: [][]): string => {
+const getGridColumnsFormula = (results2D: string[][]): string => {
   if (isBadData(results2D) || isBadData(results2D[0])) {
     return "";
   }
