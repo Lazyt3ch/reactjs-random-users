d51b9b7a4af0da62085464d726f7175929f3a041
Rashid <Lazyt3ch@gmail.com>
2021-01-18 12:15:25 +0500

    moved reducer files to /redux/reducers

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import numResultsReducer from "./numResultsReducer";
-import statusesReducer from "./statusesReducer";
-import validPropertiesReducer from "./validPropertiesReducer";
-import results2DReducer from "./results2DReducer";
-import gridColumnsFormulaReducer from "./gridColumnsFormulaReducer";
-import isBriefResultsReducer from "./isBriefResultsReducer";
-import activePageNumberReducer from "./activePageNumberReducer";
-import scrollTopArrReducer from "./scrollTopArrReducer";
-import prevPagePathReducer from "./prevPagePathReducer";
+import numResultsReducer from "./reducers/numResultsReducer";
+import statusesReducer from "./reducers/statusesReducer";
+import validPropertiesReducer from "./reducers/validPropertiesReducer";
+import results2DReducer from "./reducers/results2DReducer";
+import gridColumnsFormulaReducer from "./reducers/gridColumnsFormulaReducer";
+import isBriefResultsReducer from "./reducers/isBriefResultsReducer";
+import activePageNumberReducer from "./reducers/activePageNumberReducer";
+import scrollTopArrReducer from "./reducers/scrollTopArrReducer";
+import prevPagePathReducer from "./reducers/prevPagePathReducer";
 
 const store = configureStore({
   reducer: {
