a55dbb3f3e9b81e0ceaa1a9e9390bdccbff6f1e3
Rashid <Lazyt3ch@gmail.com>
2021-01-19 15:17:51 +0500

    changed scrollTopArrReducer

--- src/redux/reducers/scrollTopArrReducer.ts
+++ src/redux/reducers/scrollTopArrReducer.ts
@@ -1,16 +1,18 @@
 import { createReducer, PayloadAction } from "@reduxjs/toolkit"
+import { createReducer, PayloadAction } from "@reduxjs/toolkit";
 
 type ArrayOfNumbers = number[];
 
-interface ScrollTopArr {
-  scrollTopArr: ArrayOfNumbers;
-}
+// interface ScrollTopArr {
+//   scrollTopArr: ArrayOfNumbers;
+// }
 
-const initialState = { scrollTopArr: [] } as ScrollTopArr;
+// const initialState = { scrollTopArr: [] } as ScrollTopArr;
+const initialState = [] as ArrayOfNumbers;
 
 const scrollTopArrReducer = createReducer(initialState, {
   SCROLL_TOP_ARR: (state, action: PayloadAction<ArrayOfNumbers>) => {
-    state.scrollTopArr = action.payload;
+    state = action.payload;
   }
 });
 
