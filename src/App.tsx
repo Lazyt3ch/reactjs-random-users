7243c2cc8c52e3326fa38b597315e80e06f3ff3e
Rashid <Lazyt3ch@gmail.com>
2021-01-19 15:52:26 +0500

    changed App

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,7 @@ import { RootState } from "./redux/store";
 
 function App() {
   const {numResultsDefault} = constants;
+  const results2D = useSelector((state: RootState) => state.results2D);
   // const [numResults, setNumResults] = useState<number>(numResultsDefault);
   // store.dispatch({ type: actionTypes.NUM_RESULTS, payload: numResultsDefault });
   const dispatch = useDispatch();
