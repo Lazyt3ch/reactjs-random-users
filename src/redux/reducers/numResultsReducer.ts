b3e4e2525f3ea171a58a52cc985e1a2fbf73c328
Rashid <Lazyt3ch@gmail.com>
2021-01-19 13:50:06 +0500

    changed numResultsReducer

--- src/redux/reducers/numResultsReducer.ts
+++ src/redux/reducers/numResultsReducer.ts
@@ -1,10 +1,11 @@
 import { createReducer, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = { numResults: 0 };
+// const initialState = { numResults: 0 };
+const initialState = 0;
 
 const numResultsReducer = createReducer(initialState, {
   NUM_RESULTS: (state, action: PayloadAction<number>) => {
-    state.numResults = action.payload;
+    state = action.payload;
   }
 });
 
