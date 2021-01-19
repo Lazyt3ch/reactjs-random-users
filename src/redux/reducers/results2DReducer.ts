6cbd42b3d0300712f6dc8974681a41882353c515
Rashid <Lazyt3ch@gmail.com>
2021-01-19 15:15:34 +0500

    changed results2DReducer

--- src/redux/reducers/results2DReducer.ts
+++ src/redux/reducers/results2DReducer.ts
@@ -2,15 +2,17 @@ import { createReducer, PayloadAction } from "@reduxjs/toolkit"
 
 type TwoDimArrayOfStrings = string[][];
 
-interface Results2DState {
-  results2D: TwoDimArrayOfStrings;
-}
+// interface Results2DState {
+//   results2D: TwoDimArrayOfStrings;
+// }
 
-const initialState = { results2D: [] } as Results2DState;
+// const initialState = { results2D: [] } as Results2DState;
+const initialState = [] as TwoDimArrayOfStrings;
 
 const results2DReducer = createReducer(initialState, {
   RESULTS_2D: (state, action:PayloadAction<TwoDimArrayOfStrings>) => {
-    state.results2D = action.payload;
+    // state.results2D = action.payload;
+    state = action.payload;
   }
 });
 
