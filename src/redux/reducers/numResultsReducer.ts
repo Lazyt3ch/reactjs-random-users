294df5c962b1fa564cdf86699014525e97cda677
Rashid <Lazyt3ch@gmail.com>
2021-01-19 12:45:37 +0500

    working on reducers

--- src/redux/reducers/numResultsReducer.ts
+++ src/redux/reducers/numResultsReducer.ts
@@ -1,10 +1,6 @@
 import { createReducer, PayloadAction } from "@reduxjs/toolkit";
 
-interface NumResults {
-  numResults: number;
-}
-
-const initialState = { numResults: 0 } as NumResults;
+const initialState = { numResults: 0 };
 
 const numResultsReducer = createReducer(initialState, {
   NUM_RESULTS: (state, action: PayloadAction<number>) => {
