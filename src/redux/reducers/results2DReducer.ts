743761ab3a000795e676935ba2e08ace0ed9667c
Rashid <Lazyt3ch@gmail.com>
2021-01-19 10:44:00 +0500

    working on reducers

--- src/redux/reducers/results2DReducer.ts
+++ src/redux/reducers/results2DReducer.ts
@@ -1,10 +1,14 @@
 import { createReducer, PayloadAction } from "@reduxjs/toolkit"
 
-const initialState = [];
+interface TwoDimArrayOfStrings {
+  results2D: string[][];
+}
+
+const initialState = { results2D: [] };
 
 const results2DReducer = createReducer(initialState, {
-  RESULTS_2D: (state, action):PayloadAction<string[][]> => {
-    return action.payload;
+  RESULTS_2D: (state, action:PayloadAction<TwoDimArrayOfStrings>) => {
+    state.results2D = action.payload;
   }
 });
 
