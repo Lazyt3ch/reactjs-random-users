7ca2c0116ee62d6b8ca8b677035604c998bcce96
Rashid <Lazyt3ch@gmail.com>
2021-01-19 17:34:13 +0500

    cleaned up Fetcher

--- src/Components/Fetcher/Fetcher.tsx
+++ src/Components/Fetcher/Fetcher.tsx
@@ -3,106 +3,17 @@ import React from "react";
 import FetcherNumResults from "./FetcherNumResults";
 import FetcherProperties from "./FetcherProperties";
 import FetcherLaunch from "./FetcherLaunch";
-// import PropTypes from "prop-types";
 
 import "./Fetcher.css";
 
-// import store from "../../redux/store";
-
-
-// interface Props {
-//   // FetcherNumResults:
-//   numResults: number;
-//   setNumResults: React.Dispatch<React.SetStateAction<number>>;
-
-//   // FetcherProperties:
-//   allProperties: string[];
-
-//   statuses: [string, boolean][];
-//   setStatuses: React.Dispatch<React.SetStateAction<[string, boolean][]>>;
-  
-//   validProperties: string[];
-//   setValidProperties: React.Dispatch<React.SetStateAction<string[]>>;
-  
-//   // FetcherLaunch:
-//   setResults2D: React.Dispatch<React.SetStateAction<string[][]>>;
-
-//   setActivePageNumber: React.Dispatch<React.SetStateAction<number>>;
-// }
-
-// function Fetcher(props: Props) {
 function Fetcher() {
-  // const {
-  //   // FetcherNumResults:
-  //   numResults, 
-  //   setNumResults,
-
-  //   // FetcherProperties:
-  //   allProperties,
-
-  //   statuses,
-  //   setStatuses,
-    
-  //   validProperties,
-  //   setValidProperties,
-    
-  //   // FetcherLaunch:
-  //   // setResults,
-  //   setResults2D,
-
-  //   setActivePageNumber,
-  // } = props;
-
-
   return (
     <div className="fetcher">
-      <FetcherNumResults 
-        // numResults={numResults} 
-        // setNumResults={setNumResults}
-      />
-      
-      <FetcherProperties 
-        // allProperties={allProperties}
-
-        // statuses={statuses}
-        // setStatuses={setStatuses}
-        
-        // setValidProperties={setValidProperties}
-      />
-      
-      <FetcherLaunch
-        // numResults={numResults} 
-        
-        // validProperties={validProperties}
-        
-        // // setResults={setResults}
-        // setResults2D={setResults2D}
-
-        // setActivePageNumber={setActivePageNumber}
-      />
+      <FetcherNumResults />
+      <FetcherProperties />
+      <FetcherLaunch />
     </div>
   );
 }
-
-// Fetcher.propTypes = {
-//     // FetcherNumResults:
-//     numResults: PropTypes.number.isRequired,
-//     setNumResults: PropTypes.func.isRequired,
-
-//     // FetcherProperties:
-//     allProperties: PropTypes.array.isRequired,
-
-//     statuses: PropTypes.array.isRequired,
-//     setStatuses: PropTypes.func.isRequired,
-    
-//     validProperties: PropTypes.array.isRequired,
-//     setValidProperties: PropTypes.func.isRequired,
-    
-//     // FetcherLaunch:
-//     // setResults: PropTypes.func.isRequired,
-//     setResults2D: PropTypes.func.isRequired,
-
-//     setActivePageNumber: PropTypes.func.isRequired,
-// };
 
 export default Fetcher;
