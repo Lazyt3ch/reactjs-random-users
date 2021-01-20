8d0acaf67dc695fb3827885c00d1041865039519
Rashid <Lazyt3ch@gmail.com>
2021-01-20 10:45:41 +0500

    changed text in Home

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -18,8 +18,8 @@ function Home() {
           <ul>
             <li>It was built on <a href="https://reactjs.org/">React</a> using <a href="https://github.com/facebook/create-react-app">facebook/create-react-app</a> as a starting point. Initially, all of its components and helper functions were written in JavaScript (ECMAScript 2015+).</li>
             <li>Some <a href="https://material-ui.com/">Material-UI</a> components were added to ensure a more uniform look and feel when the app is opened in different browsers.</li>
-            <li>All of the components and helper functions were rewritten in TypeScript.</li>
-            <li>Redux was added as part of Redux Toolkit.</li>
+            <li>All of the components and helper functions were rewritten in <a href="https://www.typescriptlang.org/">TypeScript</a>.</li>
+            <li>Redux was added as part of <a href="https://redux-toolkit.js.org/">Redux Toolkit</a>.</li>
           </ul>
         </li>
 
