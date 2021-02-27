8b573adb20e10a8413b91202bd530846a1508fc6
Rashid <Lazyt3ch@gmail.com>
2021-02-27 10:40:54 +0500

    working on data loading from localStorage

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -36,6 +36,10 @@ const store = configureStore({
     getDefaultMiddleware().concat(save({ debounce: 500 })),
 });
 
+const loadFromStorage = () => load();
+
+window.addEventListener("storage", loadFromStorage);
+
 export default store;
 
 export type RootState = ReturnType<typeof rootReducer>;
